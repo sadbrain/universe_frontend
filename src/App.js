b6abaa22b7cf40300004c48427923bb7bf9940dd
Yam532004 <yam532004@gmail.com>
2024-05-06 16:02:12 +0700

    feat:FE/display-product and FE/dashboard

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ function App() {
                      ></Route>
                   );
                })}
-               <Route path="/category" element={<Category />} />
             </Routes>
          </div>
       </Router>
