22b2a1dd06ddc1b6bbfa5ad0961db77c2ff45a14
Yam532004 <yam532004@gmail.com>
2024-05-16 23:28:24 +0700

    fix:FE/display-product/pagination

--- src/pages/ProductList/index.js
+++ src/pages/ProductList/index.js
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './index.css';
 
 function ProductList() {
@@ -8,7 +8,43 @@ function ProductList() {
    const [showCategories, setShowCategories] = useState(false);
    const [selectedCategory, setSelectedCategory] = useState(null); // Set to null initially
    const [categories, setCategories] = useState([]); // State for categories
+   const inputRef = useRef(null); // Create a ref for the input element
 
+   const Pagination = () => {
+      return (
+         <nav aria-label="Page navigation" className="mt-5">
+            <ul className="pagination text-center maincontent-size">
+               <li className="page-item">
+                  <a className="page-link rounded-10 p-4 m-1 text-dark btn-pink" href="#">
+                     1
+                  </a>
+               </li>
+               <li className="page-item">
+                  <a className="page-link rounded-10 p-4 m-1 text-dark" href="#">
+                     2
+                  </a>
+               </li>
+               <li className="page-item">
+                  <a className="page-link rounded-10 p-4 m-1 text-dark" href="#">
+                     3
+                  </a>
+               </li>
+               <li className="page-item">
+                  <a className="page-link rounded-10 p-4 m-1 text-dark" href="#">
+                     4
+                  </a>
+               </li>
+
+               <li className="page-item">
+                  <a className="page-link rounded-10 p-4 m-1 text-dark" href="#">
+                     5
+                  </a>
+               </li>
+            </ul>
+         </nav>
+      );
+   };
+   // fetch productList default
    // Fetch categories from API
    useEffect(() => {
       const fetchCategories = async () => {
@@ -20,7 +56,16 @@ function ProductList() {
             console.error('Error fetching categories:', error);
          }
       };
-
+      const fetchProductListDefault = async () => {
+         try {
+            const response = await fetch('http://127.0.0.1:8000/api/v1/products/get-products-by-category/1/1');
+            const data = await response.json();
+            setProductList(data.data);
+         } catch (error) {
+            console.error('Error fetching products by category:', error);
+         }
+      };
+      fetchProductListDefault();
       fetchCategories();
    }, []); // Empty dependency array means this useEffect runs once after initial render
 
@@ -39,6 +84,17 @@ function ProductList() {
       }
    };
 
+   const handlePriceClick = async (price) => {
+      const enteredPrice = inputRef.current.value; // Get the value from the input field
+      console.log('Price: ', price);
+      try {
+         const response = await fetch(`http://127.0.0.1:8000/api/v1/products/get-products-by-price/${enteredPrice}/1`);
+         const data = await response.json();
+         setProductList(data.data);
+      } catch (error) {
+         console.error('Error fetching products by price:', error);
+      }
+   };
    const toggleCategoryDropdown = () => {
       setShowCategories(!showCategories); // Toggle the display state of the category list
    };
@@ -76,33 +132,42 @@ function ProductList() {
             </div>
             <div id="priceType">
                <p className="content-size">Price-type</p>
-               <input type="number" min="1" placeholder="" className="pl-5 w-100 border-pink" />
-               <button className="btn btn-pink maincontent-size text-white mt-2 rounded-10 text-center pl-5 pr-5 pt-3 pb-3 w-100">
+               <input type="number" ref={inputRef} min="1" placeholder="" className="pl-5 w-100 border-pink" />
+               <button
+                  className="btn btn-pink maincontent-size text-white mt-2 rounded-10 text-center pl-5 pr-5 pt-3 pb-3 w-100"
+                  onClick={handlePriceClick}
+               >
                   Apply
                </button>
             </div>
          </div>
          <div className="col-9">
-            <button className="btn-pink content-size text-white rounded-10 p-2 w-100">ANTIQUE DRESS</button>
+            <button className="btn-pink content-size text-white rounded-10 p-2">ANTIQUE DRESS</button>
             <div className="row mt-5 flex-item">
                {Array.isArray(productList) && productList.length > 0 ? (
                   productList.map((product) => (
-                     <div className=" card col-3 mb-5" style={{ height: '32rem' }} key={product.id}>
+                     <div className="  col-3 mb-5" style={{ height: '25rem' }} key={product.id}>
                         <div className="product bg-custom rounded-10 h-100">
                            <img className="rounded-10 w-100" src={product.thumbnail} alt={product.name} />
                            <div className="m-1 subcontent-size">
                               <div className="row">
-                                 <div className="col" style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                                 <div
+                                    className="col"
+                                    style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                                 >
                                     {product.name}
                                  </div>
-                                 <div className="col">*****</div>
+                                 <div className="col">{'*'.repeat(product.rating)}</div>
                               </div>
                               <p style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>(4.1K) Customer reviews</p>
                               <div className="row">
                                  <div className="col" style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
                                     {product.price}đ
                                  </div>
-                                 <div className="col" style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                                 <div
+                                    className="col"
+                                    style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                                 >
                                     {product.slug}
                                  </div>
                               </div>
@@ -111,10 +176,11 @@ function ProductList() {
                      </div>
                   ))
                ) : (
-                  <li className="text-center">Loading products...</li>
+                  <p className="text-center">Loading products...</p>
                )}
             </div>
          </div>
+         <Pagination />
       </div>
    );
 }
