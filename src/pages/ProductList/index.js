4a9cd94af1c068c510ba0b01d92e1246cbe8080e
Yam532004 <yam532004@gmail.com>
2024-05-21 10:34:12 +0700

    finish feat:FE/display-product

--- src/pages/ProductList/index.js
+++ src/pages/ProductList/index.js
@@ -4,6 +4,9 @@ import ProductPagination from '../../components/pagination';
 import './index.css';
 import { jsx as _jsx, jsxs as _jsxs } from 'react/jsx-runtime';
 import { RightOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import { Rate, Card } from 'antd';
+import { Cards } from '../../components/Card';
 
 function ProductList() {
    const [showCategories, setShowCategories] = useState(false);
@@ -11,22 +14,40 @@ function ProductList() {
    const toggleCategoryDropdown = () => {
       setShowCategories(!showCategories);
    };
+   const { Meta } = Card;
+
+   const productName = 'This is a very long product name that needs to be truncated or wrapped';
+
+   // Hàm này sẽ rút gọn hoặc xuống dòng tên sản phẩm nếu nó quá dài
+   const truncateProductName = (name, maxLength) => {
+      if (name.length > maxLength) {
+         return name.slice(0, maxLength) + '...';
+      }
+      return name;
+   };
+
    return (
       <div className="ml-4 mr-4">
          <div className="head-content">
-            <h1 className="text-center title-size">
+            <h1 className="text-center title-size m-3">
                <b>Fashion</b>
             </h1>
             <div className="text-center flex-item bold-text">
-               <h2>Home</h2>
-               <RightOutlined />
-               <h2 className="color-custom">Fashion </h2>
-               <RightOutlined className="color-custom" />
-               <h2>Antiques Dress</h2>
+               <Link to="/home" className="maincontent-size non-text-decoration text-dark">
+                  Home
+               </Link>
+               <RightOutlined className="m-3" />
+               <Link to="#" className="maincontent-size color-custom mr-2 non-text-decoration">
+                  Fashion
+               </Link>
+               <RightOutlined className="m-3 color-custom" />
+               <Link to="#" className="maincontent-size non-text-decoration text-dark">
+                  Antiques Dress
+               </Link>
             </div>
          </div>
-         <div className="container mt-3">
-            <div className="row flex-item">
+         <div className="container mt-5">
+            <div className="row flex-item ">
                <div className="col-3 ">
                   <div className="dropdown" id="myDropdown">
                      <button
@@ -55,7 +76,7 @@ function ProductList() {
                      )}
                   </div>
                   <div id="priceType">
-                     <p className="content-size">Price-type</p>
+                     <p className="content-size mt-4 mb-2">Price-type</p>
                      <input type="number" min="1" placeholder="" className="pl-5 w-100 border-pink" />
                      <button className="btn-pink maincontent-size text-white mt-2 rounded-10 text-center  pl-5 pr-5 pt-3 pb-3 ">
                         Apply
@@ -64,107 +85,41 @@ function ProductList() {
                </div>
                <div className="col-9">
                   <button className="btn-pink content-size text-white rounded-10 p-2">ANTIQUE DRESS</button>
-                  <div className="row mt-3">
-                     <div className="col-3  mb-5">
-                        <div className="product bg-custom rounded-10">
-                           <img
-                              className="rounded-10"
-                              src="https://cf.shopee.vn/file/1b874305dfdff03c3786983c2576d3fc"
-                              alt="Vay co trang"
-                           />
-                           <div className="m-1 maincontent-size">
-                              <div className="row">
-                                 <div className="col-6 ">Shiny Dress</div>
-                                 <div className="col-6"> *****</div>
-                              </div>
-                              <p>(4.1K) Customer reviews</p>
-                              <div className="row">
-                                 <div className="col-5">95.000đ</div>
-                                 <div className="col-7">Almost Sold out</div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <div className="col-3  mb-5">
-                        <div className="product bg-custom rounded-10">
-                           <img
-                              className="rounded-10"
-                              src="https://cf.shopee.vn/file/1b874305dfdff03c3786983c2576d3fc"
-                              alt="Vay co trang"
-                           />
-                           <div className="m-1 maincontent-size">
-                              <div className="row">
-                                 <div className="col-6 ">Shiny Dress</div>
-                                 <div className="col-6"> *****</div>
-                              </div>
-                              <p>(4.1K) Customer reviews</p>
-                              <div className="row">
-                                 <div className="col-5">95.000đ</div>
-                                 <div className="col-7">Almost Sold out</div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <div className="col-3 mb-5">
-                        <div className="product bg-custom rounded-10">
-                           <img
-                              className="rounded-10"
-                              src="https://cf.shopee.vn/file/1b874305dfdff03c3786983c2576d3fc"
-                              alt="Vay co trang"
-                           />
-                           <div className="m-1 maincontent-size">
-                              <div className="row">
-                                 <div className="col-6 ">Shiny Dress</div>
-                                 <div className="col-6"> *****</div>
-                              </div>
-                              <p>(4.1K) Customer reviews</p>
-                              <div className="row">
-                                 <div className="col-5">95.000đ</div>
-                                 <div className="col-7">Almost Sold out</div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <div className="col-3  mb-5">
-                        <div className="product bg-custom rounded-10">
-                           <img
-                              className="rounded-10"
-                              src="https://cf.shopee.vn/file/1b874305dfdff03c3786983c2576d3fc"
-                              alt="Vay co trang"
-                           />
-                           <div className="m-1 maincontent-size">
-                              <div className="row">
-                                 <div className="col-6 ">Shiny Dress</div>
-                                 <div className="col-6"> *****</div>
-                              </div>
-                              <p>(4.1K) Customer reviews</p>
-                              <div className="row">
-                                 <div className="col-5">95.000đ</div>
-                                 <div className="col-7">Almost Sold out</div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <div className="col-3  mb-5">
-                        <div className="product bg-custom rounded-10">
-                           <img
-                              className="rounded-10"
-                              src="https://cf.shopee.vn/file/1b874305dfdff03c3786983c2576d3fc"
-                              alt="Vay co trang"
-                           />
-                           <div className="m-1 maincontent-size">
-                              <div className="row">
-                                 <div className="col-6 ">Shiny Dress</div>
-                                 <div className="col-6"> *****</div>
-                              </div>
-                              <p>(4.1K) Customer reviews</p>
-                              <div className="row">
-                                 <div className="col-5">95.000đ</div>
-                                 <div className="col-7">Almost Sold out</div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
+                  <div className="row mt-4">
+                     <Cards
+                        src="https://cf.shopee.vn/file/1b874305dfdff03c3786983c2576d3fc"
+                        productName="Awesome Product"
+                        rankComments="(4.1K) Customer reviews"
+                        currentPrice="49.99"
+                        oldPrice="69.99"
+                     />
+                     <Cards
+                        src="https://cf.shopee.vn/file/1b874305dfdff03c3786983c2576d3fc"
+                        productName="Awesome Product"
+                        rankComments="(4.1K) Customer reviews"
+                        currentPrice="49.99"
+                        oldPrice="69.99"
+                     /><Cards
+                        src="https://cf.shopee.vn/file/1b874305dfdff03c3786983c2576d3fc"
+                        productName="Awesome Product"
+                        rankComments="(4.1K) Customer reviews"
+                        currentPrice="49.99"
+                        oldPrice="69.99"
+                     /><Cards
+                        src="https://cf.shopee.vn/file/1b874305dfdff03c3786983c2576d3fc"
+                        productName="Awesome Product"
+                        rankComments="(4.1K) Customer reviews"
+                        currentPrice="49.99"
+                        oldPrice="69.99"
+                     /><Cards
+                        src="https://cf.shopee.vn/file/1b874305dfdff03c3786983c2576d3fc"
+                        productName="Awesome Product"
+                        rankComments="(4.1K) Customer reviews"
+                        currentPrice="49.99"
+                        oldPrice="69.99"
+                     />
+
+                     
                   </div>
                </div>
             </div>
