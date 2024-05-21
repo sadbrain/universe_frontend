c812219e0b6c77ead3642df5d96b5ce6aafd5d46
Yam532004 <yam532004@gmail.com>
2024-05-21 21:03:12 +0700

    finish feat:FE/dashboard-revenue

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,106 +1,189 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-function Dashboard() {
-   const Chart = () => {
-      let newChart = document.getElementById('new_chart').getContext('2d');
-      // Global Options
-      Chart.defaults.global.defaultFontFamily = 'Volkhov';
-      Chart.defaults.global.defaultFontSize = 16;
-      Chart.defaults.global.defaultFontColor = '#F624ED';
+import {
+   Chart as ChartJS,
+   BarController,
+   CategoryScale,
+   LinearScale,
+   BarElement,
+   Title,
+   Tooltip,
+   Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
-      let new_massPopChart = new Chart(newChart, {
-         type: 'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-         data: {
-            labels: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            datasets: [
-               {
-                  label: 'Population',
-                  data: [250, 650, 800, 850, 900, 750, 850, 500, 980],
-                  //backgroundColor:'green',
-                  backgroundColor: [
-                     'rgba(189, 190, 198, 1)',
-                     'rgba(246, 36, 237, 1)',
-                     'rgba(189, 190, 198, 1)',
-                     'rgba(246, 36, 237, 1)',
-                     'rgba(189, 190, 198, 1)',
-                     'rgba(246, 36, 237, 1)',
-                     'rgba(189, 190, 198, 1)',
-                     'rgba(246, 36, 237, 1)',
-                     'rgba(189, 190, 198, 1)',
-                     'rgba(246, 36, 237, 1)',
-                  ],
-                  borderWidth: 1,
-                  borderColor: '#777',
-                  hoverBorderWidth: 3,
-                  hoverBorderColor: '#000',
+ChartJS.register(CategoryScale, LinearScale, BarElement, BarController, Title, Tooltip, Legend);
+
+function Dashboard() {
+   const canvasRef1 = useRef(null);
+   const canvasRef2 = useRef(null);
+   let chart1, chart2;
+
+   useEffect(() => {
+      // Create the first chart
+      const canvasElement1 = canvasRef1.current;
+      if (canvasElement1) {
+         chart1 = new ChartJS(canvasElement1.getContext('2d'), {
+            type: 'bar',
+            data: {
+               labels: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+               datasets: [
+                  {
+                     label: 'Population',
+                     data: [250, 650, 800, 850, 900, 750, 850, 500, 980],
+                     backgroundColor: [
+                        'rgba(189, 190, 198, 1)',
+                        'rgba(246, 36, 237, 1)',
+                        'rgba(189, 190, 198, 1)',
+                        'rgba(246, 36, 237, 1)',
+                        'rgba(189, 190, 198, 1)',
+                        'rgba(246, 36, 237, 1)',
+                        'rgba(189, 190, 198, 1)',
+                        'rgba(246, 36, 237, 1)',
+                        'rgba(189, 190, 198, 1)',
+                     ],
+                     borderWidth: 1,
+                     borderColor: '#777',
+                     hoverBorderWidth: 3,
+                     hoverBorderColor: '#000',
+                  },
+               ],
+            },
+            options: {
+               plugins: {
+                  title: {
+                     display: true,
+                     text: 'Chart 1',
+                     fontSize: 25,
+                  },
+                  legend: {
+                     display: true,
+                     position: 'right',
+                     labels: {
+                        color: '#000',
+                     },
+                  },
+                  tooltip: {
+                     enabled: true,
+                  },
                },
-            ],
-         },
-         options: {
-            title: {
-               display: true,
-               text: 'New',
-               fontSize: 25,
-            },
-            legend: {
-               display: true,
-               position: 'right',
-               labels: {
-                  fontColor: '#000',
+               layout: {
+                  padding: {
+                     left: 50,
+                     right: 0,
+                     bottom: 0,
+                     top: 0,
+                  },
                },
             },
-            layout: {
-               padding: {
-                  left: 50,
-                  right: 0,
-                  bottom: 0,
-                  top: 0,
+         });
+      }
+
+      // Create the second chart
+      const canvasElement2 = canvasRef2.current;
+      if (canvasElement2) {
+         chart2 = new ChartJS(canvasElement2.getContext('2d'), {
+            type: 'bar',
+            data: {
+               labels: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+               datasets: [
+                  {
+                     label: 'Population',
+                     data: [250, 650, 800, 850, 900, 750, 850, 500, 980],
+                     backgroundColor: [
+                        'rgba(189, 190, 198, 1)',
+                        'rgba(246, 36, 237, 1)',
+                        'rgba(189, 190, 198, 1)',
+                        'rgba(246, 36, 237, 1)',
+                        'rgba(189, 190, 198, 1)',
+                        'rgba(246, 36, 237, 1)',
+                        'rgba(189, 190, 198, 1)',
+                        'rgba(246, 36, 237, 1)',
+                        'rgba(189, 190, 198, 1)',
+                     ],
+                     borderWidth: 1,
+                     borderColor: '#777',
+                     hoverBorderWidth: 3,
+                     hoverBorderColor: '#000',
+                  },
+               ],
+            },
+            options: {
+               plugins: {
+                  title: {
+                     display: true,
+                     text: 'Chart 2',
+                     fontSize: 25,
+                  },
+                  legend: {
+                     display: true,
+                     position: 'right',
+                     labels: {
+                        color: '#000',
+                     },
+                  },
+                  tooltip: {
+                     enabled: true,
+                  },
+               },
+               layout: {
+                  padding: {
+                     left: 50,
+                     right: 0,
+                     bottom: 0,
+                     top: 0,
+                  },
                },
             },
-            tooltips: {
-               enabled: true,
-            },
-         },
-      });
-   };
+         });
+      }
+
+      // Cleanup function to destroy the chart instances
+      return () => {
+         if (chart1) {
+            chart1.destroy();
+         }
+         if (chart2) {
+            chart2.destroy();
+         }
+      };
+   }, []);
    return (
       <div className="container">
          <div className="row ml-10 mr-10">
             <div className="col-1"></div>
             <div className="col-3 maincontent-size bold-text">
                <div className="bg-custom rounded-10 text-center mt-5 p-3">
-                  <p className="text-white">Total price</p>
+                  <p className="text-white m-2">Total price</p>
                   <p>30,000,000 USD</p>
                </div>
                <div className="bg-custom rounded-10 text-center mt-5 p-3">
-                  <p className="text-white ">Current monthly </p>
+                  <p className="text-white m-2">Current monthly</p>
                   <p>20,000,000 USD</p>
                </div>
                <div className="bg-custom rounded-10 text-center mt-5 p-3">
-                  <p className="text-white ">Total orders </p>
+                  <p className="text-white m-2">Total orders</p>
                   <p>5,000</p>
                </div>
                <div className="bg-custom rounded-10 text-center mt-5 p-3">
-                  <p className="text-white ">Monthly orders </p>
+                  <p className="text-white m-2">Monthly orders</p>
                   <p>2,000</p>
                </div>
                <div className="bg-custom rounded-10 text-center mt-5 p-3">
-                  <p className="text-white ">Total</p>
+                  <p className="text-white m-2">Total</p>
                   <p>2,000</p>
                </div>
                <div className="bg-custom rounded-10 text-center mt-5 p-3">
-                  <p className="text-white ">Monthly</p>
+                  <p className="text-white m-2">Monthly</p>
                   <p>2,000</p>
                </div>
             </div>
             <div className="col-7">
-               <canvas id="revenue_chart"></canvas>
-               <canvas id="new_chart"></canvas>
+               <canvas ref={canvasRef1} id="chart1"></canvas>
+               <canvas ref={canvasRef2} id="chart2"></canvas>
             </div>
             <div className="col-1"></div>
          </div>
-         <Chart />
-         <Chart />
       </div>
    );
 }
