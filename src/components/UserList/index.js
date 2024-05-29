3647e0d5b6bd082ea3114e177c11de8173feb49a
Yam532004 <yam532004@gmail.com>
2024-05-29 23:40:00 +0700

    feat:fetch user and feat:lock-unlock account

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -3,23 +3,44 @@ import Swal from 'sweetalert2';
 import { EditFilled, DeleteFilled } from '@ant-design/icons';
 import { Switch } from 'antd';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function UserList(props) {
-   const { userName, email, phoneNumber } = props;
+   const { userName, email, phoneNumber, id, locked } = props;
+   const [isLocked, setIsLocked] = useState(locked);
+   const onChange = async (checked) => {
+      try {
+         localStorage.setItem(
+            'token',
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC92MVwvYXV0aFwvbG9naW4iLCJpYXQiOjE3MTY5OTg0MDksImV4cCI6MTcxNzAwMjAwOSwibmJmIjoxNzE2OTk4NDA5LCJqdGkiOiJIZng1Szd6akE0ZUx1MHNqIiwic3ViIjoxLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.-xasqUYyd0dIJHNLbMKhxAmvsWFoKoburA9DzSv-_EQ',
+         );
+         const token = localStorage.getItem('token');
+         const axiosInstance = axios.create({
+            baseURL: 'http://127.0.0.1:8000/api/v1',
+            headers: {
+               Authorization: `Bearer ${token}`,
+            },
+         });
 
-   const onChange = (checked) => {
-      console.log(`switch to ${checked}`);
+         const res = await axiosInstance.get(`/users/lock-unclock/${id}`);
+         const data = res.data.data;
+
+         setIsLocked(data.locked);
+      } catch (error) {
+         console.error(error);
+      }
    };
-   const SwitchLocked = () => <Switch defaultChecked onChange={onChange} />;
+   const SwitchLocked = () => <Switch defaultChecked={isLocked} onChange={onChange} />;
    return (
       <tr>
          <td>{userName}</td>
          <td>{email}</td>
          <td>{phoneNumber}</td>
          <td>
-            <SwitchLocked className='d-flex justify-content-center align-items-center' />
+            <SwitchLocked className="d-flex justify-content-center align-items-center" />
          </td>
-         <td className='d-flex justify-content-center align-items-center'>
+         <td className="d-flex justify-content-center align-items-center">
             <button className="btn btn-primary m-2">
                <Link to="/admin/user/edit" className="maincontent-size text-white text-decoration-none">
                   <EditFilled />
@@ -34,7 +55,6 @@ function UserList(props) {
                         customClass: {
                            confirmButton: 'btn btn-success ',
                            cancelButton: 'btn btn-danger m-2',
-                           
                         },
                         buttonsStyling: false,
                      });
@@ -68,7 +88,7 @@ function UserList(props) {
                         });
                   }}
                >
-                  <DeleteFilled  />
+                  <DeleteFilled />
                   Delete
                </Link>
             </button>
--- src/pages/UserManagement/ListUser/index.js
+++ src/pages/UserManagement/ListUser/index.js
@@ -2,7 +2,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import UserList from '../../../components/UserList';
 import { Link } from 'react-router-dom';
 import './index.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function ListUser() {
+   const [users, setUsers] = useState([]);
+   useEffect(() => {
+      try {
+         localStorage.setItem(
+            'token',
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC92MVwvYXV0aFwvbG9naW4iLCJpYXQiOjE3MTY5OTg0MDksImV4cCI6MTcxNzAwMjAwOSwibmJmIjoxNzE2OTk4NDA5LCJqdGkiOiJIZng1Szd6akE0ZUx1MHNqIiwic3ViIjoxLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.-xasqUYyd0dIJHNLbMKhxAmvsWFoKoburA9DzSv-_EQ',
+         );
+         const token = localStorage.getItem('token');
+         const axiosInstance = axios.create({
+            baseURL: 'http://127.0.0.1:8000/api/v1',
+            headers: {
+               Authorization: `Bearer ${token}`,
+            },
+         });
+         const userManagers = async () => {
+            const res = await axiosInstance.get('/users');
+            const data = res.data.data;
+            setUsers(data);
+            console.log('Data', users);
+         };
+         userManagers();
+      } catch (error) {
+         console.error(error);
+      }
+   }, []);
+
    return (
       <div className="container">
          <h1 className="color-custom text-center title-size">User list</h1>
@@ -15,19 +43,19 @@ function ListUser() {
          <table className="table table-bordered">
             <thead>
                <tr className="color-custom content-size border-dark">
-                  <th style={{ color:"#ff6699" }}>User name</th>
-                  <th style={{ color:"#ff6699" }}>Email</th>
-                  <th style={{ color:"#ff6699" }}>Phone</th>
-                  <th style={{ color:"#ff6699" }}>Lock-Unlock</th>
-                  <th style={{ color:"#ff6699" }}>Action</th>
+                  <th style={{ color: '#ff6699' }}>User name</th>
+                  <th style={{ color: '#ff6699' }}>Email</th>
+                  <th style={{ color: '#ff6699' }}>Phone</th>
+                  <th style={{ color: '#ff6699' }}>Lock-Unlock</th>
+                  <th style={{ color: '#ff6699' }}>Action</th>
                </tr>
             </thead>
             <tbody>
-               <UserList userName="Tinh Tran" email="example@example.com" phoneNumber="12345678" />
-               <UserList userName="Am" email="example@example.com" phoneNumber="12345678" />
-               <UserList userName="Nhung" email="example@example.com" phoneNumber="12345678" />
-               <UserList userName="Duyen" email="example@example.com" phoneNumber="12345678" />
-               <UserList userName="Ngan" email="example@example.com" phoneNumber="12345678" />
+              {
+               users.map ((user) =>(
+                  <UserList id= {user.id} userName={user.name} email={user.email} phoneNumber={user.phone} locked={user.locked} />
+               ))
+              }
             </tbody>
          </table>
       </div>
