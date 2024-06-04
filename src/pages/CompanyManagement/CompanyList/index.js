b8fbed77f8ab9356e9db67a995a0e76737823a90
ThuyNgan015 <ngan.ho25@student.passerellesnumeriques.org>
2024-06-04 21:48:53 +0700

    feat:handle api crud company-management

--- src/pages/CompanyManagement/CompanyList/index.js
+++ src/pages/CompanyManagement/CompanyList/index.js
@@ -87,12 +87,52 @@ function CompanyList() {
               <td>{company.phone_number}</td>
               <td className="button-cell">
                 <div className="button-wrapper">
-                  <button
-                    className="delete-btn"
-                    onClick={() => handleDelete(company.id)}
-                  >
-                    <FaTrashAlt /> Delete
-                  </button>
+                <Link
+                  className="maincontent-size text-white text-decoration-none"
+                  onClick={() => {
+                  const swalWithBootstrapButtons = Swal.mixin({
+                    customClass: {
+                    confirmButton: 'btn btn-success ',
+                    cancelButton: 'btn btn-danger m-2',
+                    },
+                    buttonsStyling: false,
+                  });
+
+                  swalWithBootstrapButtons
+                  .fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: 'Yes, delete it!',
+                    cancelButtonText: 'No, cancel!',
+                    reverseButtons: true,
+                  })
+                  .then((result) => {
+                    if (result.isConfirmed) {
+                    handleDelete(company.id);
+                    swalWithBootstrapButtons.fire({
+                    title: 'Deleted!',
+                    text: 'Your file has been deleted.',
+                    icon: 'success',
+                    });
+                    } else if (
+                  /* Read more about handling dismissals below */
+                      result.dismiss === Swal.DismissReason.cancel
+                      ) {
+                    swalWithBootstrapButtons.fire({
+                    title: 'Cancelled',
+                    text: 'Your imaginary file is safe :)',
+                    icon: 'error',
+                  });
+                }
+              });
+              }}
+              >
+                <button className="delete-btn">
+                <FaTrashAlt /> Delete
+                </button>
+                  </Link>
                   <Link to={`/admin/company/update/${company.id}`} className="no-underline">
                     <button className="edit-btn">
                       <FaEdit /> Edit
